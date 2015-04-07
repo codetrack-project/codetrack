6aed72a134f7d6a0c95e63b3992bebba840402d6
josecmoj <josecmoj@gmail.com>
2015-04-07 16:16:36 -0300

    Create sample classes

---
+++ codetrack-annotation/src/main/java/org/codetrack/annotation/SampleAnnotation.java
@@ -0,0 +1,7 @@
+package org.codetrack.annotation;
+
+/**
+ * Created by josecmoj on 07/04/15.
+ */
+public @interface SampleAnnotation {
+}
---
+++ codetrack-scan/src/main/java/ScanApplication.java
@@ -0,0 +1,5 @@
+/**
+ * Created by josecmoj on 07/04/15.
+ */
+public class ScanApplication {
+}
