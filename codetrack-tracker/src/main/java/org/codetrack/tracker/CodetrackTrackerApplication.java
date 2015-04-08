ed81f3078eea1c39e3dafc5c710bc090fa75ac1d
josecmoj <josecmoj@gmail.com>
2015-04-07 23:03:56 -0300

    Create codetrack-tracker

---
+++ codetrack-tracker/src/main/java/org/codetrack/tracker/CodetrackTrackerApplication.java
@@ -0,0 +1,12 @@
+package org.codetrack.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetrackTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodetrackTrackerApplication.class, args);
+    }
+}
