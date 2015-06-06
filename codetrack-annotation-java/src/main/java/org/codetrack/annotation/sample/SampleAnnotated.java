87d4bb715424f8cb4b87d6d7dbd2506aa6f66bce
josecmoj <josecmoj@gmail.com>
2015-06-06 13:51:55 -0300

    Create Cycles, Iterations and Sprints type and package annotations

--- codetrack-annotation-java/src/main/java/org/codetrack/annotation/sample/SampleAnnotated.java
+++ codetrack-annotation-java/src/main/java/org/codetrack/annotation/sample/SampleAnnotated.java
@@ -1,67 +1,77 @@
 /*
- * Copyright 2015 the original author or authors members of codetrack.org
+ *  Copyright 2015 the original author or authors members of codetrack.org
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  */
 
 package org.codetrack.annotation.sample;
 
-import org.codetrack.annotation.*;
+import org.codetrack.annotation.behavior.Rule;
+import org.codetrack.annotation.behavior.Rules;
+import org.codetrack.annotation.definition.*;
+import org.codetrack.annotation.fixing.Fix;
+import org.codetrack.annotation.fixing.Fixes;
+import org.codetrack.annotation.identify.Product;
+import org.codetrack.annotation.identify.Reference;
+import org.codetrack.annotation.temporal.Iteration;
 
 /**
  * Sample annotate class
  *
  * Created by josecmoj on 14/04/15.
  */
-@Product(value = "RELEASE 02-70", usecases = {
-        @UseCase(value = "UC1978", revisions = {})})
-@Artifacts(artifacts = {"application.properties", "methodRule.xhtml"})
+@Product(id = "RELEASE 02-70",
+        usecases = @UseCases({
+                @UseCase(id = "UC1978", revisions = {})}))
+@Reference(id = "CONFIGFILE", value = "application.properties")
 @Requirements({
-        @Requirement(value = "RQ2032", story = @Story("ST0123")),
-        @Requirement("RQ345")
+        @Requirement(id = "RQ2032", story = @Story(id = "ST0123")),
+        @Requirement(id = "RQ345")
 })
+@Iteration(id = "IT012")
 public class SampleAnnotated {
 
-    @Rule(value = "RN12345", usecase = @UseCase("UC1978"))
-    @Artifacts(artifacts = {"application.properties"})
+    @Rule(id = "RN12345", usecase = @UseCase(id = "UC1978"))
+    @Reference(id = "CONFIGFILE")
     public void methodRule(){
         System.out.println("methodRule");
     }
 
-    @Rule(value = "RN23452", usecase = @UseCase("UC1989"))
-    @Fix(id = "4567", type = FixType.EXP, observation = "This fix is necessary...")
+    @Rule(id = "RN23452", usecase = @UseCase(id = "UC1989"))
+    @Fix(id = "4567", type = "EXP", observation = "This fixing is necessary...")
     public void methodRuleAndFix(){
         System.out.println("methodRuleAndFix");
     }
 
     @Rules({
-            @Rule(value = "RN23434", usecase = @UseCase("UC1978")),
-            @Rule(value = "RN23431", usecase = @UseCase("UC1989"))
+            @Rule(id = "RN23434", usecase = @UseCase(id = "UC1978")),
+            @Rule(id = "RN23431", usecase = @UseCase(id = "UC1989"))
     })
     public void methodMultiRules(){
         System.out.println("methodMultiRules");
     }
 
     @Fixes({
-            @Fix(id = "2882", type = FixType.SLA)
+            @Fix(id = "2882", type = "SLA", iteration = @Iteration(id = "IT012"))
     })
     public void methodMultiFixes(){
         System.out.println("methodMultiFixes");
     }
 
-
+    @Iteration(id = "IT012")
     @Reference(id = "ESPEC234", values = {"INTEGRATION SYS.DOC", "PAGE 10"})
-    @Feature(value = "FEAT-089", description = "Demonstrate the Feature tag", story = @Story("ST0123"))
+    @Feature(id = "FEAT-089", description = "Demonstrate the Feature tag", story = @Story(id = "ST0123"))
     public void methodFeature() {
         System.out.println("methodFeature");
     }
